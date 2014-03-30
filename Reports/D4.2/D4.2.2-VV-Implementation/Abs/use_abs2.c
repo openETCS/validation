ac0012bcb7ed6221133df1892e2b34fe3e7eead5
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-03-30 10:42:33 +0200

    add another set of examples

---
+++ Reports/D4.2/D4.2.2-VV-Implementation/Abs/use_abs2.c
@@ -0,0 +1,28 @@
+
+#include "abs2.h"
+
+
+void use_1()
+{
+  int a = abs_int(3);
+  int b = abs_int(-1);
+  int c = abs_int(INT_MAX);
+  int d = abs_int(INT_MIN);
+}
+
+
+void use_2(int x)
+{
+  int a = abs_int(x);
+}
+
+/*@
+    requires (10 <= x < 100) || (-200 < x < -50);
+*/
+void use_3(int x)
+{
+  int a = abs_int(x);
+  //@ assert a >= 10;
+}
+
+

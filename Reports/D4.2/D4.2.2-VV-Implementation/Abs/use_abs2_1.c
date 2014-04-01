310bbe08d04a8f5cbb0763a280217e1d7bf6be1b
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-01 21:17:52 +0200

    add examples to section on Frama-C

---
+++ Reports/D4.2/D4.2.2-VV-Implementation/Abs/use_abs2_1.c
@@ -0,0 +1,14 @@
+
+#include "abs2.h"
+
+
+void use_1()
+{
+  int a = abs_int(3);
+  int b = abs_int(-1);
+  int c = abs_int(INT_MAX);
+  int d = abs_int(INT_MIN);
+}
+
+
+
---
+++ Reports/D4.2/D4.2.2-VV-Implementation/Abs/use_abs2_2.c
@@ -0,0 +1,8 @@
+
+#include "abs2.h"
+
+void use_2(int x)
+{
+  int a = abs_int(x);
+}
+

266dbe85f40c16d0b1cb214292db017cd97da8dd
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-06 16:13:44 +0200

    provide very simple contract to check absence of rte

--- Reports/D4.2/D4.2.2-VV-Implementation/Abs/abs1b.c
+++ Reports/D4.2/D4.2.2-VV-Implementation/Abs/abs1b.c
@@ -2,9 +2,7 @@
 #include <limits.h>
 
 /*@
-    ensures 0 <= x           ==>  \result ==  x;
-    ensures 0 > x > INT_MIN  ==>  \result == -x;
-    ensures x == INT_MIN     ==>  \result == INT_MIN;
+    requires x != INT_MIN;
 */
 int abs_int(int x)
 {

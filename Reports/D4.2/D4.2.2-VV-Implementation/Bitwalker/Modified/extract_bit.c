7e5e477c39f16b0413993dec7cb200600a2f8a53
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-07 14:59:01 +0200

    refactor

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified/extract_bit.c
+++
@@ -1,8 +0,0 @@
-#include "Bitwalker.h"
-#include <stdint.h>
-
-uint64_t extract_bit(uint8_t Bitstream[], unsigned int index)
-{
-    return (Bitstream[index >> 3] & BitwalkerBitMaskTable[index & 0x07] != 0);
-}
-
--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified/extract_bit.h
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified/extract_bit.h
@@ -1,3 +1,6 @@
 
+uint64_t extract_bit(uint8_t Bitstream[], unsigned int index)
+{
+  return (Bitstream[index >> 3] & BitwalkerBitMaskTable[index & 0x07]) != 0;
+}
 
-uint64_t extract_bit(uint8_t Bitstream[], unsigned int index);

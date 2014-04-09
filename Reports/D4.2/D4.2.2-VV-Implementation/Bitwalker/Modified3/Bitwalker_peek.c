34a3805e3eca977a4ecc0449f85d03d5feb5afc3
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-09 21:54:16 +0200

    add more invariants

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/Bitwalker_peek.c
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/Bitwalker_peek.c
@@ -1,10 +1,27 @@
 #include "Bitwalker.h"
+#include "limits.h"
 
+/*@
+    requires d > 0;
+
+    assigns \nothing;
+
+    ensures 0 <= \result < d;
+*/
 unsigned int inverse_modulo(unsigned int n, unsigned int d)
 {
     return d - 1 - (n % d);
 }
 
+/*@
+   requires 8 * BitstreamSizeInBytes < UINT_MAX;
+   requires Startposition + Length < UINT_MAX;
+   requires 0 <= Length < 64;
+   requires \valid(Bitstream + (0..BitstreamSizeInBytes-1));
+
+   assigns \nothing;
+
+*/
 uint64_t Bitwalker_Peek(unsigned int Startposition,
                         unsigned int Length,
                         uint8_t Bitstream[],
@@ -15,10 +32,17 @@ uint64_t Bitwalker_Peek(unsigned int Startposition,
 
   uint64_t retval = 0;
 
+  /*@
+    loop invariant Startposition <= i <= Startposition + Length;
+    loop assigns i, retval;
+    loop variant Startposition + Length - i;
+  */
   for (unsigned int i = Startposition; i < Startposition + Length; i++)
   {
         unsigned int bit_index  = inverse_modulo(i, 8);
         uint8_t bit_as_byte = (Bitstream[i / 8] >> bit_index) & 1;
+        //@ assert bit_as_byte <= 1;
+
         retval = 2 * retval + bit_as_byte;
   }
 

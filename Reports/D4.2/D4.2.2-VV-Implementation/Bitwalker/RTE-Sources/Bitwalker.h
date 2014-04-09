80d4a936f0cc8492b8f83ae712081cb8fb43f32b
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-09 19:20:07 +0200

    copy "original" sources

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/RTE-Sources/Bitwalker.h
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/RTE-Sources/Bitwalker.h
@@ -2,7 +2,7 @@
 #define BITWALKER_H
 
 #include <stdint.h>
-#include <limits.h>
+
 
 static const uint8_t BitwalkerBitMaskTable[] = {0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01};
 
@@ -12,13 +12,12 @@ uint64_t Bitwalker_Peek(unsigned int Startposition,
                         unsigned int BitstreamSizeInBytes);
 
 
+
 int      Bitwalker_Poke(unsigned int Startposition,
                         unsigned int Length,
                         uint8_t Bitstream[],
                         unsigned int BitstreamSizeInBytes,
                         uint64_t Value);
 
+
 #endif // BITWALKER_H
-
-
-
---
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/RTE-Sources/Bitwalker_peek.c
@@ -0,0 +1,24 @@
+#include "Bitwalker.h"
+
+uint64_t Bitwalker_Peek(unsigned int Startposition,
+                        unsigned int Length,
+                        uint8_t Bitstream[],
+                        unsigned int BitstreamSizeInBytes)
+{
+  if (((Startposition + Length - 1) >> 3) >= BitstreamSizeInBytes)
+    return 0;	// error: index out of range
+
+  uint64_t retval = 0;
+
+  unsigned int i;
+
+  for (i = Startposition; i < Startposition + Length; i++)
+  {
+    uint8_t CurrentValue = Bitstream[i >> 3] &
+                           BitwalkerBitMaskTable[i & 0x07];
+
+    retval = (retval << 1) + (uint8_t)(CurrentValue != 0);
+  }
+
+  return retval;
+}

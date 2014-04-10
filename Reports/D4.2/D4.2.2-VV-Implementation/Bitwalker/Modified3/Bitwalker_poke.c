a7cca9506978d3d9715c408014769165fbf276d1
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-10 19:22:06 +0200

    some simplifications of for poke

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/Bitwalker_poke.c
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/Bitwalker_poke.c
@@ -1,24 +1,27 @@
 #include "Bitwalker.h"
 #include "poke_bits.h"
 
-int Bitwalker_Poke (unsigned int Startposition, unsigned int Length, uint8_t Bitstream[], unsigned int BitstreamSizeInBytes, uint64_t Value)
+int Bitwalker_Poke (unsigned int Startposition,
+                    unsigned int Length,
+                    uint8_t Bitstream[],
+                    unsigned int BitstreamSizeInBytes,
+                    uint64_t Value)
 {
   // plausibility check: is last byte in range
-  if (((Startposition + Length - 1) >> 3) >= BitstreamSizeInBytes)
+  if ((Startposition + Length)  > 8 * BitstreamSizeInBytes)
     return -1;	// error: index out of range
 
   // plausibility check: is value in range
-  uint64_t MaxValue = (((uint64_t)0x01) << Length) - 1;
+  uint64_t MaxValue = (((uint64_t) 1) << Length) - 1;
 
   if (MaxValue < Value)
     return -2;  // error: value to big for bit field
 
   // Everything ok, we can iterate bitwise from left to right
-  int i;
-
-  for (i = Startposition + Length - 1; i >= (int)Startposition; i--)
+  for (int i = Length - 1; i >= 0; i--)
   {
-    poke_bits(&Bitstream[0], i, Value);
+    int pos = Startposition + i;
+    poke_bits(&Bitstream[0], pos, Value);
     Value >>= 1;
   }
 

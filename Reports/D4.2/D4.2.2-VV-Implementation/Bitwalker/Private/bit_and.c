2e7dbd4315d269e28c3793183dbb3bbb32451e67
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-11 18:34:16 +0200

    add example for unsigned long

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Private/bit_and.c
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Private/bit_and.c
@@ -12,9 +12,15 @@ void bit_and_short(unsigned short a)
    //@ assert b == 0 || b == 1;
 }
 
-void bit_and(unsigned int a)
+void bit_and_int(unsigned int a)
 {
    unsigned int b = a & 1;
+   //@ assert b == 0 || b == 1;
+}
+
+void bit_and_long(unsigned long a)
+{
+   unsigned long b = a & 1;
    //@ assert b == 0 || b == 1;
 }
 

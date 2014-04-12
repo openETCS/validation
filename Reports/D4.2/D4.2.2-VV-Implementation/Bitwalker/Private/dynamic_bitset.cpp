bdfc6a0511d68c261f2389b25b810f398d597a09
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-12 20:37:50 +0200

    print bits according to bitwalker

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Private/dynamic_bitset.cpp
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Private/dynamic_bitset.cpp
@@ -15,7 +15,24 @@ void print(size_t width, size_t value)
 
 int main()
 {
-   for(size_t i = 0; i < 16; ++i)
-       print(6, i);
+   //for(size_t i = 0; i < 9; ++i)
+       //print(10, i);
+
+   std::vector<uint8_t> bytes(4);
+   bytes[1] = 13;
+
+   size_t start = 8;
+   size_t length = 8;
+   for(size_t i = 0; i < length; ++i)
+   {
+      size_t pos = start + i;
+      size_t byte_index = pos/8;
+      size_t bit_index = 7 - (pos % 8);
+      uint8_t shift = bytes[byte_index] >> bit_index;
+      std::cout << byte_index << "\t" 
+                << bit_index << "\t" 
+                << (shift & 1) << "\t" 
+                << std::endl;
+   }
 }
 

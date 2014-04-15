878087335941c9ec07f93f9183d7d76181a9b4f6
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-15 20:23:48 +0200

    add new version for nested loops

---
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified4/Bitwalker_Peek.c
@@ -0,0 +1,35 @@
+#include "Bitwalker_Peek.h"
+
+uint64_t Bitwalker_Peek(unsigned int Start,
+                        unsigned int Length,
+                        uint8_t Bitstream[],
+                        unsigned int BitstreamSize)
+{
+  if ((Start + Length)  > 8 * BitstreamSize)
+    return 0;	// error: index out of range
+
+  uint64_t retval = 0;
+
+  /*@
+    loop invariant 0 <= i <= Length;
+    loop invariant 0 <= retval < 1 << i;
+    loop assigns i, retval;
+    loop variant Length - i;
+  */
+  for (unsigned int i = 0; i < Length; i++)
+  {
+    unsigned int pos = Start + i;
+    unsigned int byte_index = pos / 8;
+    unsigned int bit_index  = inverse_modulo(pos, 8);
+
+    // treat as unsigned int for Frama-C
+    unsigned int shift = Bitstream[byte_index] >> bit_index;
+    unsigned int bit_as_byte = shift & 1;
+    //@ assert bit_as_byte == 0 || bit_as_byte == 1;
+
+    retval = 2 * retval + bit_as_byte;
+  }
+
+  return retval;
+}
+

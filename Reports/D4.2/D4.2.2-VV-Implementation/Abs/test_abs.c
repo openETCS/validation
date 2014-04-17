7d6335f697cb88484fd40d90eff9961309091dfe
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-04-17 09:32:43 +0200

    describe steps of demo

--- Reports/D4.2/D4.2.2-VV-Implementation/Abs/test_abs.c
+++ Reports/D4.2/D4.2.2-VV-Implementation/Abs/test_abs.c
@@ -6,18 +6,23 @@ extern int abs_int(int);
 
 void print_abs(int x)
 {
-  printf("%d\t\t%d\n", x, abs_int(x));
+  printf("%12d\t\t%12d\n", x, abs_int(x));
 }
 
 int main()
 {
+  printf("\n");
   print_abs(0);
 
+  printf("\n");
   print_abs(1);
   print_abs(10);
   print_abs(INT_MAX);
 
+  printf("\n");
   print_abs(-1);
   print_abs(-10);
   print_abs(INT_MIN);
+
+  printf("\n");
 }

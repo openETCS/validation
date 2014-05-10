4cbfc7d47120e1adaf383fefdef3ac1f4ab1b7d3
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-05-10 16:39:04 +0200

    add behaviors to PokeNext

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/PokeNext.h
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/PokeNext.h
@@ -12,9 +12,28 @@
   requires Locals->CurrentBitposition + Length <= UINT_MAX;
   requires Locals->CurrentBitposition + Length <= 8 * Locals->Length;
 
-  assigns  Locals->CurrentBitposition;
+  assigns  Locals->Bitstream[Locals->CurrentBitposition/8..
+                            (Locals->CurrentBitposition + Length)/8];
 
-  ensures  \result < (1 << Length);
+  behavior  invalid_bit_sequence:
+    assumes (Locals->CurrentBitposition + Length)  > 8 * Locals->Length;
+    assigns \nothing;
+    ensures \result == -1;
+
+  behavior  value_too_big:
+    assumes (1 << Length) <= Value &&
+            (Locals->CurrentBitposition + Length) <= 8 * Locals->Length;
+    assigns \nothing;
+    ensures \result == -2;
+
+  behavior  normal_case:
+    assumes Value < (1 << Length) &&
+            (Locals->CurrentBitposition + Length) <= 8 * Locals->Length;
+   assigns  Locals->Bitstream[Locals->CurrentBitposition/8..
+                             (Locals->CurrentBitposition + Length)/8];
+
+  complete behaviors;
+  disjoint behaviors;
 */
 int  Bitwalker_IncrementalWalker_Poke_Next(
        T_Bitwalker_Incremental_Locals*  Locals,

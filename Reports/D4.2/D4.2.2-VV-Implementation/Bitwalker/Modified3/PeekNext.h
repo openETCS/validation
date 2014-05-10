ac61f8cb786dbba5ab5597ac5ef1840e22419de3
Jens Gerlach <jens.gerlach@fokus.fraunhofer.de>
2014-05-10 10:54:28 +0200

    first version of contract of PeekNext

--- Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/PeekNext.h
+++ Reports/D4.2/D4.2.2-VV-Implementation/Bitwalker/Modified3/PeekNext.h
@@ -3,7 +3,18 @@
 #define PeekNext_included
 
 #include "Locals.h"
+#include "ValidIncrementalLocals.h"
 
+/*@
+  requires \valid(Locals);
+  requires Valid(Locals);
+  requires 0 <= Length <= 64;
+  requires Locals->CurrentBitposition + Length <= UINT_MAX;
+
+  assigns \nothing;
+
+  ensures \result < (1 << Length);
+*/
 uint64_t Bitwalker_IncrementalWalker_Peek_Next(
   T_Bitwalker_Incremental_Locals*  Locals,
   unsigned int                     Length);

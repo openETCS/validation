7357bb724aa57764e0e25a6cd9ac3a4374cb0025
janWelte <j.Welte@tu-braunschweig.de>
2015-06-22 17:40:55 +0200

    Clean Up of old files

--- VnVUserStories/VnVUserStoryTUBS/05-Work/CheckBGInputChannel/Simulation/Make.bat
+++
@@ -1,6 +0,0 @@
-@SET PATH=C:/Program Files (x86)/Esterel Technologies/SCADE R15.2/SCADE/../contrib/Msys/mingw/bin;%PATH%
-@SET INCLUDE=C:/Program Files (x86)/Esterel Technologies/SCADE R15.2/SCADE/../contrib/Msys/mingw/include
-@SET LIB=C:/Program Files (x86)/Esterel Technologies/SCADE R15.2/SCADE/../contrib/Msys/mingw/lib
-@IF "-B"=="%1" SET ARG=%1
-gcc -print-search-dirs
-@mingw32-make.exe %ARG%
